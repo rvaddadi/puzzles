b91e5c280e566a41e557c028b3fd4dd1be8ce1b4
Leandro Facchinetti <leafac@gmail.com>
2012-10-08 21:17:09 -0300

    5.2

--- cracking-the-coding-interview/5.2/decimal-to-binary.rb
+++ cracking-the-coding-interview/5.2/decimal-to-binary.rb
@@ -1,4 +1,34 @@
 def decimal_to_binary(n)
-  integer_part = n.floor
-  decimal_part = n - integer_part
+  result = ''
+
+  integer_part, decimal_part = n.split('.').map(&:to_i)
+
+  while integer_part > 0
+    result.prepend (integer_part  % 2).to_s
+    integer_part /= 2
+  end
+
+  decimal_digits = 1
+  decimal_part   = "0.#{ decimal_part.to_s }".to_f
+  result << '.'
+
+  while decimal_part > 0
+    return 'ERROR' if decimal_digits > 10
+
+    factor = 1.0 / 2**decimal_digits
+
+    result << if decimal_part >= factor
+                decimal_part -= factor
+                '1'
+              else
+                '0'
+              end
+
+    decimal_digits += 1
+  end
+
+  result
 end
+
+puts decimal_to_binary('3.5')
+puts decimal_to_binary('0.14285714285')

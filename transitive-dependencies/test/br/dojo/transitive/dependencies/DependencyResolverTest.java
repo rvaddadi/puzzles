17ecbb26b6f21e6c544ecc6a2e2dd9e740c3256d
Leandro Facchinetti <leafac@gmail.com>
2012-10-10 09:25:34 -0300

    Init Transitive Dependencies

---
+++ transitive-dependencies/test/br/dojo/transitive/dependencies/DependencyResolverTest.java
@@ -0,0 +1,14 @@
+package br.dojo.transitive.dependencies;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DependencyResolverTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

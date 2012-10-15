b7672d0f8aa5b1c3cf7c1e55e7d4363fb4da7234
Leandro Facchinetti <leafac@gmail.com>
2012-10-15 00:02:29 -0300

    8.7 solution from book

--- cracking-the-coding-interview/8.7/Representations.java
+++ cracking-the-coding-interview/8.7/Representations.java
@@ -3,16 +3,15 @@ public class Representations {
 	public static int representations(int n) {
 		return representations(n, 25);
 	}
-	
+
 	private static int representations(int n, int max) {
 		int representations;
-		
+
 		representations = 0;
-		
+
 		if (n == 0) {
 			representations = 1;
-		}
-		else if (n > 0) {
+		} else if (n > 0) {
 			if (25 <= max && n >= 25) {
 				representations += representations(n - 25, 25);
 			}
@@ -26,8 +25,31 @@ public class Representations {
 				representations += representations(n - 1, 1);
 			}
 		}
-		
+
 		return representations;
+	}
+
+	// solution from book to confirm my answer
+	public static int makeChange(int n, int denom) {
+		int next_denom = 0;
+		switch (denom) {
+		case 25:
+			next_denom = 10;
+			break;
+		case 10:
+			next_denom = 5;
+			break;
+		case 5:
+			next_denom = 1;
+			break;
+		case 1:
+			return 1;
+		}
+		int ways = 0;
+		for (int i = 0; i * denom <= n; i++) {
+			ways += makeChange(n - i * denom, next_denom);
+		}
+		return ways;
 	}
 
 	public static void main(String[] args) {
@@ -36,5 +58,8 @@ public class Representations {
 		representations(8);
 		representations(10);
 		representations(11);
+
+		representations(100);
+		makeChange(100, 25);
 	}
 }

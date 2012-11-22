bb4300d99e64a12251889afa50ee6c4a74d3ea01
Leandro Facchinetti <leafac@gmail.com>
2012-11-22 11:51:55 -0200

    Game of Life in Javacript - Refactor in tests

--- game-of-life/test/game.of.life.test.js
+++ game-of-life/test/game.of.life.test.js
@@ -4,6 +4,11 @@ var GameOfLife = require('../lib/game.of.life.js');
 
 describe('GameOfLife', function(){
 
+  var assertNextGenerationGrid = function(grid, nextGenerationGrid) {
+    assert.deepEqual(nextGenerationGrid,
+      new GameOfLife(grid).nextGeneration().getGrid());
+  };
+
   it('should have a grid', function(){
     var grid = [
       [0, 0, 0],
@@ -11,72 +16,72 @@ describe('GameOfLife', function(){
       [0, 0, 0]
     ];
 
-    assert.deepEqual(new GameOfLife(grid).getGrid(), grid);
+    assert.deepEqual(grid, new GameOfLife(grid).getGrid());
   });
 
   it('should kill cells with less then 2 neighbors', function() {
-    assert.deepEqual(new GameOfLife([
-        [0, 0, 0],
-        [0, 1, 0],
-        [0, 0, 0]
-      ]).nextGeneration().getGrid(), [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-      ]);
-    assert.deepEqual(new GameOfLife([
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 0, 0]
-      ]).nextGeneration().getGrid(), [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-      ]);
+    assertNextGenerationGrid([
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0]
+    ], [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ]);
+    assertNextGenerationGrid([
+      [0, 1, 0],
+      [0, 1, 0],
+      [0, 0, 0]
+    ], [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0]
+    ]);
   });
 
   it('should kill cells with more then 3 neighbors', function() {
-    assert.deepEqual(new GameOfLife([
-        [0, 1, 0],
-        [1, 1, 1],
-        [0, 1, 0]
-      ]).nextGeneration().getGrid(), [
-        [1, 1, 1],
-        [1, 0, 1],
-        [1, 1, 1]
-      ]);
-    assert.deepEqual(new GameOfLife([
-        [1, 0, 1],
-        [0, 1, 0],
-        [1, 0, 1]
-      ]).nextGeneration().getGrid(), [
-        [0, 1, 0],
-        [1, 0, 1],
-        [0, 1, 0]
-      ]);
+    assertNextGenerationGrid([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 1, 0]
+    ], [
+      [1, 1, 1],
+      [1, 0, 1],
+      [1, 1, 1]
+    ]);
+    assertNextGenerationGrid([
+      [1, 0, 1],
+      [0, 1, 0],
+      [1, 0, 1]
+    ], [
+      [0, 1, 0],
+      [1, 0, 1],
+      [0, 1, 0]
+    ]);
   });
 
   it('should keep cells with 2 or 3 neighbors', function() {
-    assert.deepEqual(new GameOfLife([
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 0, 0]
-      ]).nextGeneration().getGrid(), [
-        [0, 1, 1],
-        [0, 1, 1],
-        [0, 0, 0]
-      ]);
+    assertNextGenerationGrid([
+      [0, 1, 0],
+      [0, 1, 1],
+      [0, 0, 0]
+    ], [
+      [0, 1, 1],
+      [0, 1, 1],
+      [0, 0, 0]
+    ]);
   });
 
   it('should give life to cells with 3 neighbors', function() {
-    assert.deepEqual(new GameOfLife([
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 0, 0]
-      ]).nextGeneration().getGrid(), [
-        [0, 1, 1],
-        [0, 1, 1],
-        [0, 0, 0]
-      ]);
+    assertNextGenerationGrid([
+      [0, 1, 0],
+      [0, 1, 1],
+      [0, 0, 0]
+    ], [
+      [0, 1, 1],
+      [0, 1, 1],
+      [0, 0, 0]
+    ]);
   });
 });

438b26f508f56abbf33cab6bb9c95297caceb642
Leandro Facchinetti <leafac@gmail.com>
2013-02-13 21:00:08 -0200

    Mais uma função passando

--- author-names/author-names.c
+++ author-names/author-names.c
@@ -7,24 +7,44 @@ char* toupperString(char* originalString) {
 	char* upperString = (char*) malloc(sizeof(char) * strlen(originalString));
 	int i;
 
-	if (strlen(originalString) == 0) {
-		return originalString;
-	}
-
 	for (i = 0; i < strlen(originalString); ++i) {
-		upperString[i] = toupper(originalString[i]);	
-
+		upperString[i] = toupper(originalString[i]);
 	}
 
 	return upperString;
 }
 
+char* formatRegularName(char* regularName) {
+	char* formattedRegularName = (char*) malloc(sizeof(char) * strlen(regularName));
+	int i;
+
+	for (i = 0; i < strlen(regularName); ++i) {
+		if (i == 0) {
+			formattedRegularName[i] = toupper(regularName[i]);
+		}
+		else {
+			formattedRegularName[i] = tolower(regularName[i]);
+		}
+	}
+
+	return formattedRegularName;
+}
+
 int main() {
+	printf("\n\ntoupperString()\n");
+
 	printf("expected: \"\" actual: \"%s\"\n", toupperString(""));
 	printf("expected: \"N\" actual: \"%s\"\n", toupperString("N"));
 	printf("expected: \"N\" actual: \"%s\"\n", toupperString("n"));
-	printf("expected: \"NICOLAS\" actual: \"%s\"\n", toupperString("nicolas"));
+	printf("expected: \"TESTE\" actual: \"%s\"\n", toupperString("teste"));
 
+	printf("\n\nformatRegularName()\n");
+
+	printf("expected: \"\" actual: \"%s\"\n", formatRegularName(""));
+	printf("expected: \"R\" actual: \"%s\"\n", formatRegularName("R"));
+	printf("expected: \"Teste\" actual: \"%s\"\n", formatRegularName("teste"));
+	// http://www.coisinha.com.br/miguxeitor/
+	printf("expected: \"Testi\" actual: \"%s\"\n", formatRegularName("tesTI"));
 
 	return 0;
 }
